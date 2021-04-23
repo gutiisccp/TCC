05c17869e3ca9f04860399cb1ac609964eb74e07
gutiisccp <gabriel.gbogones@outlook.com>
2021-04-23 01:47:53 -0300

    TCC Atualizado

--- js/scripts.js
+++ js/scripts.js
@@ -183,3 +183,18 @@ $( document ).ready(function() {
     });
   
   });
+
+  (() => {
+    if (!localStorage.pureJavaScriptCookies) {
+      document.querySelector(".box-cookies").classList.remove('hide');
+    }
+    
+    const acceptCookies = () => {
+      document.querySelector(".box-cookies").classList.add('hide');
+      localStorage.setItem("pureJavaScriptCookies", "accept");
+    };
+    
+    const btnCookies = document.querySelector(".btn-cookies");
+    
+    btnCookies.addEventListener('click', acceptCookies);
+  })();
